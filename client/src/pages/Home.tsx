f8bdcdcd34459ee56059c9f478d14c62846b9243
Kgiruc <kgiruc76@gmail.com>
2023-05-08 14:47:20 +0200

    some tests init and react router dom

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default Home;
--- client/src/test/App.test.tsx
+++ client/src/test/App.test.tsx
@@ -1,12 +1,13 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
+import Home from '../pages/Home';
 
 describe('App', () => {
   it('Renders hello world', () => {
     // ARRANGE
-    render(<App />);
+    render(<Home />);
     // ACT
     // EXPECT
     expect(
@@ -14,5 +15,17 @@ describe('App', () => {
         level: 1,
       })
     ).toHaveTextContent('hello world');
+  });
+  it('renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/banana']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('NotFound');
   });
 });
