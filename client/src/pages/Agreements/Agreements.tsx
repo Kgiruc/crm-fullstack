ace93955dfc2456740f713fdeedc8c1ce7711a77
Kgiruc <kgiruc76@gmail.com>
2023-06-11 10:17:30 +0200

    agreements rtk query

--- client/src/pages/Agreements/Agreements.tsx
+++ client/src/pages/Agreements/Agreements.tsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+import { useAgreementsQuery } from "./services/agreementsApi";
+import AgreementsList from "./components/AgreementsList";
+
 function Agreements() {
-  return <div>Agreements</div>;
+  const { data, error, isLoading, isSuccess, isFetching } = useAgreementsQuery();
+  return <section>
+  <h1>All Agreements</h1>
+  {isLoading && <p>loading...</p>}
+  {isFetching && <p>fetching</p>}
+  {error && <p>error</p>}
+  {isSuccess && (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Customer Name</th>
+            <th>Agreement</th>
+            <th>date sign</th>
+            <th>date end</th>
+            <th>value</th>
+            <th>description</th>
+          </tr>
+        </thead>
+        <AgreementsList agreements={data} />
+      </table>
+      <Link to="/agreements/add">Add Agreement</Link>
+    </>
+  )}
+</section>;
 }
 
 export default Agreements;
