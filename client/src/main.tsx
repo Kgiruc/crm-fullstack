1f7eb68ec0d7c2e50ba42c14e96ed20f6d717801
Kgiruc <kgiruc76@gmail.com>
2023-11-25 12:14:17 +0100

    add colors css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import './reset.css';
+import './styles/reset.css';
 import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
